997dcab783a938356ec58aeac7ab6074a1e466fb
Valerie Bragg <vbragg@ufledu>
2019-07-02 23:26:54 -0400

    Add img hover effect with JavaScript, testing click effect

--- src/main.js
+++ src/main.js
@@ -1,8 +1,24 @@
 const hoverImage = document.getElementById('show-message');
 const hiddenMessage = document.getElementById('hiddenmessage');
+const changeColor = document.getElementsByTagName('img');
 
 const showName = () => {
   hiddenMessage.classList.remove('hide');
 }
 
+const hideName = () => {
+  hiddenMessage.classList.add('hide');
+}
+
+const newBorder = () => {
+  changeColor.classList.add('orange');
+  changeColor.removeAttribute('id');
+}
+
+
+
 hoverImage.addEventListener('mouseover', showName);
+
+hoverImage.addEventListener('mouseleave', hideName);
+
+changeColor.addEventListener('click', newBorder);
